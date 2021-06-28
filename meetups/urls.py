ac455452a0c89620d64d379c2592cd53540dba1d
allyboy08 <alexsassman65@gmail.com>
2021-06-28 16:09:04 +0200

    update

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('meetups/', views.index) #our-domain.com/meetups
+    path('meetups/', views.index, name='all-meetups'), #our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'), # our-domain.com/meetups/<dynamic-path-segment>
 ]
